a5f1085568fb618c63b77ee30fa8f391b46644b2
Bjay02 <bolajiafolabi02@gmail.com>
2023-01-24 10:29:31 +0100

    updates

---
+++ js/main.js
@@ -0,0 +1,11 @@
+// const myImage = document.getElementById("hero_img")
+
+
+// myImage.onclick = () => {
+//     const mySrc = myImage.getAttribute("src");
+//     if (mySrc === 'assets/img/slide2.jpg') {
+//         myImage.setAttribute('src', 'assets/img/slide1.jpg')
+//     } else {
+//         myImage.setAttribute('src', 'assets/img/slide2.jpg')
+//     }
+// }
